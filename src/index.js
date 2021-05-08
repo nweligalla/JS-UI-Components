8cd48e44b0902ece1b35abfb2d40bc0545c298da
nweligalla <nayanaweligalla@gmail.com>
2021-05-08 17:12:19 +0530

    dropdown

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import Tooltip from './ui/tooltip';
 import Dropdown from './ui/dropdown';
+import Tabs from './ui/tabs';
 
 
 //create tooltip
-
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
 
 
 //create dropdown
-
 const dropdowns = document.querySelectorAll('.dropdown');
 
 dropdowns.forEach(dropDown => {
     const instance = new Dropdown(dropDown)
     instance.init();
 });
+
+//create tab
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
